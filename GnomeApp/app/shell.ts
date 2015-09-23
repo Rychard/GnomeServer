1ebb03a6423aecef8b7b9084785548a2585279be
Joshua Shearer <joshua.a.shearer@gmail.com>
2015-09-23 15:06:28 -0400

    Temporarily fix TypeScript compilation error.

--- GnomeApp/app/shell.ts
+++ GnomeApp/app/shell.ts
@@ -1,9 +1,14 @@
 ﻿import router = require('plugins/router');
 
+// TODO: Find out if there's a more appropriate way to resolve these issues until the TypeScript type definitions (.d.ts) can be updated.
+interface DurandalRouteConfigurationExtras extends DurandalRouteConfiguration {
+    [x: string]: any;
+}
+
 class Shell {
     router: DurandalRouter = router;
     activate = () => {
-        var configs: DurandalRouteConfiguration[] = [
+        var configs: DurandalRouteConfigurationExtras[] = [
             { route: '',            moduleId: 'home/index',     title: 'Home',      nav: 1,     settings: { icon: 'fa fa-home fa-lg' } },
             { route: 'gnome',       moduleId: 'gnome/index',    title: 'Gnomes',    nav: 2,     settings: { icon: 'fa fa-users fa-lg' } },
             { route: 'military',    moduleId: 'military/index', title: 'Military',  nav: 3,     settings: { icon: 'fa fa-shield fa-lg' } },
